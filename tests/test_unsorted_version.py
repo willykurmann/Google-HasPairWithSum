f8109a4c753596e4db8bcedb27a04c4057512ef8
wkurmann <willy.kurmann@gmail.com>
2018-03-05 22:42:34 +0100

    Added unsorted test for both functions

--- tests/test_unsorted_version.py
+++ tests/test_unsorted_version.py
@@ -1,15 +1,16 @@
 from unsorted_version import has_pair_with_sum
 
-''' Should test the data vector doesnt contain a pair that adds up to the sum
-'''
 def test_data_vector_doesnt_contain_pair_with_sum():
     data = [1,2,3,9]
     sum = 8
     assert has_pair_with_sum(data,sum) == False
 
-''' Should test the data vector does contain a pair that adds up to the sum
-'''
 def test_data_vector_does_contain_pair_with_sum():
     data = [1,2,4,4]
+    sum = 8
+    assert has_pair_with_sum(data,sum) == True
+
+def test_unsorted_data_vector():
+    data = [4,4,1,2]
     sum = 8
     assert has_pair_with_sum(data,sum) == True
